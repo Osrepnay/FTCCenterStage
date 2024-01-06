bcabb5d3b3422caa9c22310c036f5f165bc2c60a
Llerraf Gnem <lleerraaff@gmail.com>
2024-01-06 00:54:40 -0600

    variable prop squares
    allows the detection square to vary, picks the best-scoring position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/Propecessor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/Propecessor.java
@@ -8,8 +8,12 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.vision.VisionProcessor;
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Range;
 import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
@@ -18,15 +22,21 @@ import java.util.List;
 public class Propecessor implements VisionProcessor {
 
     private final Telemetry telemetry;
-    Rect left;
-    Rect center;
-    public boolean isRed = true;
+    Range leftXRange;
+    int leftHeight;
+    Range centerXRange;
+    int centerHeight;
+    public boolean isRed = false;
     public double detectThreshold = 50;
-    public double unsavoryChannelFactor = 1;
+    public double unsavoryChannelFactor = 0.5;
 
     public enum Spike {
         LEFT, CENTER, RIGHT
     }
+
+    public Spike selectedSpike;
+    private Rect spikeLocLeft;
+    private Rect spikeLocCenter;
 
     public Propecessor(Telemetry telemetry) {
         this.telemetry = telemetry;
@@ -34,28 +44,39 @@ public class Propecessor implements VisionProcessor {
 
     @Override
     public void init(int width, int height, CameraCalibration calibration) {
-        left = new Rect(50, 40, 150, 160);
-        center = new Rect(420, 5, 105, 112);
+        leftXRange = new Range(120, 235);
+        leftHeight = 120;
+        centerXRange = new Range(440, 545);
+        centerHeight = 110;
     }
 
-    private double processSide(Mat subframe) {
+    private Mat.Tuple2<Double> processSide(Mat subframe, int sectionHeight) {
         List<Mat> channels = new ArrayList<>();
         Core.split(subframe, channels);
         // put coi at first position
         if (!isRed) {
             channels.add(0, channels.get(2));
         }
-        channels.get(1).convertTo(channels.get(1), -1, unsavoryChannelFactor);
-        channels.get(2).convertTo(channels.get(2), -1, unsavoryChannelFactor);
+        Rect roi = new Rect(subframe.width() / 2, sectionHeight / 2, 1, subframe.height() - sectionHeight);
+        Mat kernel = new Mat(sectionHeight, subframe.width(), CvType.CV_64F, new Scalar(1.0 / subframe.width() / sectionHeight * unsavoryChannelFactor));
+        Imgproc.filter2D(new Mat(channels.get(1), roi), channels.get(1), CvType.CV_64F, kernel, new Point(-1, -1), 0, Core.BORDER_CONSTANT);
+        Imgproc.filter2D(new Mat(channels.get(2), roi), channels.get(2), CvType.CV_64F, kernel, new Point(-1, -1), 0, Core.BORDER_CONSTANT);
+        kernel.setTo(new Scalar(1.0 / subframe.width() / sectionHeight));
+        Imgproc.filter2D(new Mat(channels.get(0), roi), channels.get(0), CvType.CV_64F, kernel, new Point(-1, -1), 0, Core.BORDER_CONSTANT);
         Core.subtract(channels.get(0), channels.get(1), channels.get(0));
         Core.subtract(channels.get(0), channels.get(2), channels.get(0));
-        return Core.mean(channels.get(0)).val[0];
+        Core.MinMaxLocResult res = Core.minMaxLoc(channels.get(0));
+        return new Mat.Tuple2<>(res.maxVal, res.maxLoc.y);
     }
 
     @Override
     public Object processFrame(Mat frame, long captureTimeNanos) {
-        double avgLeft = processSide(frame.submat(left).clone());
-        double avgCenter = processSide(frame.submat(center).clone());
+        Mat.Tuple2<Double> leftRes = processSide(frame.submat(new Rect(leftXRange.start, 0, leftXRange.size(), frame.height())).clone(), leftHeight);
+        double avgLeft = leftRes.get_0();
+        spikeLocLeft = new Rect(leftXRange.start, (int) (double) leftRes.get_1(), leftXRange.size(), leftHeight);
+        Mat.Tuple2<Double> centerRes = processSide(frame.submat(new Rect(centerXRange.start, 0, centerXRange.size(), frame.height())).clone(), centerHeight);
+        double avgCenter = centerRes.get_0();
+        spikeLocCenter = new Rect(centerXRange.start, (int) (double) centerRes.get_1(), centerXRange.size(), centerHeight);
         telemetry.addData("left", avgLeft);
         telemetry.addData("center", avgCenter);
         Spike select;
@@ -67,7 +88,11 @@ public class Propecessor implements VisionProcessor {
                 select = Spike.LEFT;
             }
         } else {
-            select = Spike.RIGHT;
+            if (avgCenter > detectThreshold) {
+                select = Spike.CENTER;
+            } else {
+                select = Spike.RIGHT;
+            }
         }
         telemetry.addData("spike", select);
         telemetry.update();
@@ -84,11 +109,15 @@ public class Propecessor implements VisionProcessor {
 
     @Override
     public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+        selectedSpike = (Spike) userContext;
         Paint paint = new Paint();
         paint.setColor(Color.GREEN);
         paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(10);
-        canvas.drawRect(convertRects(scaleRect(left, scaleBmpPxToCanvasPx)), paint);
-        canvas.drawRect(convertRects(scaleRect(center, scaleBmpPxToCanvasPx)), paint);
+        paint.setStrokeWidth(5);
+        canvas.drawRect(convertRects(scaleRect(new Rect(leftXRange.start, 0, leftXRange.size(), 480), scaleBmpPxToCanvasPx)), paint);
+        canvas.drawRect(convertRects(scaleRect(new Rect(centerXRange.start, 0, centerXRange.size(), 480), scaleBmpPxToCanvasPx)), paint);
+        paint.setColor(Color.RED);
+        canvas.drawRect(convertRects(scaleRect(spikeLocLeft, scaleBmpPxToCanvasPx)), paint);
+        canvas.drawRect(convertRects(scaleRect(spikeLocCenter, scaleBmpPxToCanvasPx)), paint);
     }
 }
